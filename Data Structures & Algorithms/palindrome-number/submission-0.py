4b0128fb2874478a9bb6580a61d1cc072f1168c8
John Judge <141768082+johnjjudge@users.noreply.github.com>
2026-06-19 21:56:54 -0700

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        strX = str(x)
+        l = 0
+        r = len(strX) - 1
+        while l < r:
+            if strX[l] != strX[r]:
+                return False
+            l += 1
+            r -=1
+        return True
